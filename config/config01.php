11013c1cc89937f24503b054fbfd6c2f8b9bf0a0
DiLamilla <lamilla.16@outlook.com>
2024-09-26 09:25:04 -0500

    inicio

---
+++ config/config01.php
@@ -0,0 +1,10 @@
+
+<?php
+/*--------- variables para la conección con la base de datos --------------*/ 
+const DB_HOST = 'localhost';
+const DB_NAME = '';
+const DB_USER = '';
+const DB_PASSWORD = '';
+//--------- ingresar el link del proyecto -----------------------//
+const DB_URL = '';
+?>
